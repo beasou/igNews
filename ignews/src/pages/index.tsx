dc9f6cc52fa3fd7d2cd2af172f678f9fa998612c
beasou <beatrizreginasouza1@gmail.com>
2022-06-15 18:05:50 -0300

    Estilização com SASS

--- ignews/src/pages/index.tsx
+++ ignews/src/pages/index.tsx
@@ -1,7 +1,9 @@
-
+import styles from '../styles/home.module.scss'
 
 export default function Home() {
   return (
-   <h1>hello word</h1>
+   <h1 className={styles.title}>
+     hello <span>word</span> 
+  </h1>
   )
 }
